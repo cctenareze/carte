54ec93b253b7bfb66fd736458834c06608eaf0cb
cctenareze <63282554+cctenareze@users.noreply.github.com>
2020-04-23 16:59:16 +0200

    Add files via upload

---
+++ data/Transport_1.js
@@ -0,0 +1,10 @@
+var json_Transport_1 = {
+"type": "FeatureCollection",
+"name": "Transport_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "code posta": "32100", "commune": "CONDOM", "qui": "TENAREZE AMBULANCES", "quoi": null, "catégorie": "Transport", "obs": "Propose un service gratuit tous les jours : récupérer les médicaments à la pharmacie et les courses commandées pour les personnes seules et affaiblies", "adresse": "Rue des arènes", "numéro": "09 70 35 14 69", "mail": null, "Site": "https:\/\/www.tenareze-taxi-ambulance.com\/\n" }, "geometry": { "type": "Point", "coordinates": [ 0.364544818698439, 43.956033979933387 ] } },
+{ "type": "Feature", "properties": { "code posta": "32250", "commune": "MONTREAL-DU-GERS", "qui": "TRANSPORTS CLAUDINE", "quoi": null, "catégorie": "Transport", "obs": "Transport de personnes \/ Livraison à domicile de commandes du magasin au domicile du client", "adresse": "Séviac", "numéro": "05 62 28 67 92", "mail": null, "Site": "https:\/\/www.claudine32.com\/\n" }, "geometry": { "type": "Point", "coordinates": [ 0.18023224874757, 43.941378883503553 ] } },
+{ "type": "Feature", "properties": { "code posta": "32100", "commune": "CONDOM", "qui": "AUTOCARS BAJOLLE", "quoi": null, "catégorie": "Transport", "obs": "Fonctionnement des lignes régulières (depuis Condom vers Agen, Auch et Barbotan). Les lignes d'Occitanie et d'Aquitaine fonctionnent également", "adresse": "2, Rue des Artisans", "numéro": "05 62 28 17 55", "mail": null, "Site": null }, "geometry": { "type": "Point", "coordinates": [ 0.361353881480716, 43.94628090768844 ] } }
+]
+}
